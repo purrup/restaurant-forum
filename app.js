483254c38a6e8fde2baf3cde02eb1355da140af2
Jerry Tsai <purrup27@gmail.com>
2019-06-06 21:55:18 +0800

    add user valisation and error messages

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const db = require('./models')
 const port = 3000
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
