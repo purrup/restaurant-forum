2a813393a3ac189927dee103fd009eac40b35a72
Jerry Tsai <purrup27@gmail.com>
2019-06-08 00:58:32 +0800

    change view engine from handlebars to pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,32 +8,25 @@ let userController = {
   },
 
   signUp: (req, res) => {
-    // confirm password
-    if (req.body.passwordCheck !== req.body.password) {
-      req.flash('error_messages', '兩次密碼輸入不同！')
-      return res.redirect('/signup')
-    } else {
-      // confirm unique user
-      User.findOne({ where: { email: req.body.email } }).then(user => {
-        if (user) {
-          req.flash('error_messages', '信箱重複！')
-          return res.redirect('/signup')
-        } else {
-          User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(
-              req.body.password,
-              bcrypt.genSaltSync(10),
-              null
-            ),
-          }).then(user => {
-            req.flash('success_messages', '成功註冊帳號！')
-            return res.redirect('/signin')
-          })
-        }
-      })
-    }
+    // confirm unique user
+    User.findOne({ where: { email: req.body.email } }).then(user => {
+      if (user) {
+        res.send({ msg: '信箱已經註冊過！' })
+      } else {
+        User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: bcrypt.hashSync(
+            req.body.password,
+            bcrypt.genSaltSync(10),
+            null
+          ),
+        }).then(user => {
+          // req.flash('success_messages', '成功註冊帳號！')
+          return res.redirect('/signin')
+        })
+      }
+    })
   },
 }
 
---
+++ public/javascripts/signupValidation.js
@@ -0,0 +1,60 @@
+const form = document.querySelector('.form-signin')
+const name = document.querySelector('#name')
+const email = document.querySelector('#email')
+const emailFeedback = document.querySelector('#email~.invalid-feedback')
+const password = document.querySelector('#password')
+const passwordCheck = document.querySelector('#password-check')
+
+const passwordCheckFeedback = document.querySelector(
+  '#password-check~.invalid-feedback'
+)
+let errorMessage = ''
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  // 如果驗證沒過，加上.was-validated
+  if (form.checkValidity() === false) {
+    form.classList.add('was-validated')
+  } else {
+    // 如果驗證有過，移除.was-validated
+    form.classList.remove('was-validated')
+    if (password.value !== passwordCheck.value) {
+      passwordCheckFeedback.innerHTML = '兩次密碼輸入不同！'
+      passwordCheck.classList.add('is-invalid')
+      return
+    }
+
+    signUp()
+  }
+})
+
+async function signUp() {
+  try {
+    let userData = new Object()
+    userData.name = name.value
+    userData.email = email.value
+    userData.password = password.value
+    console.log('test', JSON.stringify(userData))
+
+    const response = await axios('/signup', {
+      method: 'POST',
+      data: userData,
+    })
+    // email有註冊過
+    if (response.data.msg) {
+      email.classList.add('is-invalid')
+      emailFeedback.innerHTML = response.data.msg
+    } else {
+      // form.submit()
+      window.location('/signin')
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+form.addEventListener('input', e => {
+  if (e.target.matches('.is-invalid')) {
+    e.target.classList.remove('is-invalid')
+  }
+})
