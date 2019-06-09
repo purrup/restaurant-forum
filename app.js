86e385b46e8e6dbbf8dbf01a241f24e0d14cf4c8
Jerry Tsai <purrup27@gmail.com>
2019-06-09 13:24:32 +0800

    add process.env.PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('./models')
-const port = 3000
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
