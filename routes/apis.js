b1fb751507e1a49e60a4d4c17b941ccd0a19ed99
Jerry Tsai <purrup27@gmail.com>
2019-07-14 16:01:30 +0800

    add signIn api

--- routes/apis.js
+++ routes/apis.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 const adminController = require('../controllers/api/adminController.js')
+const userController = require('../controllers/api/userController.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
@@ -15,5 +16,7 @@ router.get('/admin/categories/:id', adminController.getCategories)
 router.post('/admin/categories',adminController.postCategory)
 router.put('/admin/categories/:id',adminController.putCategory)
 router.delete('/admin/categories/:id',adminController.deleteCategory)
+// JWT signin
+router.post('/signin', userController.signIn)
 
 module.exports = router
