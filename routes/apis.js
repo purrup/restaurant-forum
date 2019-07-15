761c7e263736c599a727fb9dd63dc99a998b4135
Jerry Tsai <purrup27@gmail.com>
2019-07-15 20:31:17 +0800

    add signUp api

--- routes/apis.js
+++ routes/apis.js
@@ -4,18 +4,30 @@ const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 const adminController = require('../controllers/api/adminController.js')
 const userController = require('../controllers/api/userController.js')
+const passport = require('../config/passport')
+const authenticated = passport.authenticate('jwt', { session: false })
 
-router.get('/admin/restaurants', adminController.getRestaurants)
-router.get('/admin/restaurants/:id', adminController.getRestaurant)
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
-router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
-router.get('/admin/restaurants/:id/edit',adminController.editRestaurant)
-router.put('/admin/restaurants/:id',upload.single('image'),adminController.putRestaurant)
-router.get('/admin/categories',adminController.getCategories)
-router.get('/admin/categories/:id', adminController.getCategories)
-router.post('/admin/categories',adminController.postCategory)
-router.put('/admin/categories/:id',adminController.putCategory)
-router.delete('/admin/categories/:id',adminController.deleteCategory)
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+router.post('/signup', userController.signUp)
+
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
+router.get('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.getRestaurant)
+router.delete('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.deleteRestaurant)
+router.post('/admin/restaurants', upload.single('image'), authenticated, authenticatedAdmin, adminController.postRestaurant)
+router.get('/admin/restaurants/:id/edit', authenticated, authenticatedAdmin,adminController.editRestaurant)
+router.put('/admin/restaurants/:id',upload.single('image'), authenticated, authenticatedAdmin,adminController.putRestaurant)
+router.get('/admin/categories', authenticated, authenticatedAdmin,adminController.getCategories)
+router.get('/admin/categories/:id', authenticated, authenticatedAdmin, adminController.getCategories)
+router.post('/admin/categories', authenticated, authenticatedAdmin,adminController.postCategory)
+router.put('/admin/categories/:id', authenticated, authenticatedAdmin,adminController.putCategory)
+router.delete('/admin/categories/:id', authenticated, authenticatedAdmin,adminController.deleteCategory)
 // JWT signin
 router.post('/signin', userController.signIn)
 
